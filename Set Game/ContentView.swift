16af02e1f7a49e59de65d01be5dea6ffe1fb5676
Jia Yee <jiayee_koe@mymail.sutd.edu.sg>
2020-09-10 12:14:17 +0800

    initial commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Koe Jia-Yee on 10/9/20.
+//  Copyright © 2020 Koe Jia-Yee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
